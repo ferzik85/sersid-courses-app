036ba46c9915add0178bbce9c057e6bc3d7b788b
ferzik85 <sersid@bk.ru>
2023-11-07 17:59:00 +0100

    style course info component

--- src/components/CourseInfo/CourseInfo.jsx
+++ src/components/CourseInfo/CourseInfo.jsx
@@ -3,37 +3,39 @@ import Button from '../../common/Button/Button';
 import formatDate from '../../utils/FormatDate';
 import formatDuration from '../../utils/FormatDuration';
 import formatAuthors from '../../utils/FormatAuthors';
+import classes from './CourseInfo.module.css';
 
 function CourseInfo({ course }) {
 	return (
-		<div>
-			<p>{course.title}</p>
-			<div>
-				<p>Description</p>
-				<p>{course.description}</p>
+		<div className={classes['course-info']}>
+			<div className={classes['course-info-title']}>{course.title}</div>
+			<div className={classes['course-description']}>
+				<div>Description:</div>
+				<div className={classes['course-description-layout']}>
+					<div className={classes['course-description-text']}>{course.description}</div>
+					<div>
+						<p>
+							<span>ID: </span>
+							{course.id}
+						</p>
+						<p>
+							<span>Duration: </span>
+							{formatDuration(course.duration)}
+						</p>
+						<p>
+							<span>Created: </span>
+							{formatDate(course.creationDate)}
+						</p>
+						<p>
+							<span>Authors: </span>
+							{formatAuthors(course.authors)}
+						</p>
+					</div>
+				</div>
 			</div>
 			<div>
-				<p>
-					<span>ID: </span>
-					{course.id}
-				</p>
-				<p>
-					<span>Duration: </span>
-					{formatDuration(course.duration)}
-				</p>
-				<p>
-					<span>Created: </span>
-					{formatDate(course.creationDate)}
-				</p>
-				<p>
-					<span>Authors: </span>
-					{formatAuthors(course.authors)}
-				</p>
-				<p>
-					<Button label='BACK' />
-				</p>
+				<Button label='BACK' />
 			</div>
-			<div />
 		</div>
 	);
 }
