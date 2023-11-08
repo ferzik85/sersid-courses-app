e976d7670c15931522927764fe66d8841410f81e
ferzik85 <sersid@bk.ru>
2023-11-08 20:23:19 +0100

    save changes

--- src/common/Input/Input.jsx
+++ src/common/Input/Input.jsx
@@ -4,7 +4,7 @@ import classes from './Input.module.css';
 function Input({ onChange }) {
 	return (
 		<div className={classes.input}>
-			<input type='text' placeholder='Search course' onChange={(e) => onChange(e.target.value)} />
+			<input type='text' placeholder='Search course ...' onChange={(e) => onChange(e.target.value)} />
 		</div>
 	);
 }
