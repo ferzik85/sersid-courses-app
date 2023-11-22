6927e0e68b8d83d9ae0485d89b5d6de288035342
ferzik85 <sersid@bk.ru>
2023-11-22 10:23:55 +0100

    PR feedback part 2

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Button from '../../common/Button/Button';
 import LabeledInput from '../../common/LabeledInput/LabeledInput';
-import Header from '../Header/Header';
 import { validateInput, validateEmail, validatePassword } from '../../utils/ValidateInput';
 import registerUserAsync from '../../api/RegisterUser';
 
@@ -51,26 +50,23 @@ function Registration() {
 	}
 
 	return (
-		<>
-			<Header />
-			<div className={styles.reg}>
-				<b className={styles.regHeader}>Registration</b>
-				<div className={styles.regBody}>
-					<form onSubmit={handleSubmit} id={formId} className={styles.regForm}>
-						<LabeledInput name='Name' isInvalid={nameIsInvalid} onChange={handleNameChange} inputClassName={styles.regInput} />
-						<LabeledInput name='Email' isInvalid={emailIsInvalid} onChange={handleEmailChange} inputClassName={styles.regInput} />
-						<LabeledInput name='Password' isInvalid={passwordIsInvalid} onChange={handlePasswordChange} inputClassName={styles.regInput} />
-					</form>
-					<Button label='REGISTER' type='submit' formName={formId} className={styles.regButton} />
-					<div className={styles.regHelp}>
-						If you have an account you may{' '}
-						<Link to='/login'>
-							<b>Login</b>
-						</Link>
-					</div>
+		<div className={styles.reg}>
+			<b className={styles.regHeader}>Registration</b>
+			<div className={styles.regBody}>
+				<form onSubmit={handleSubmit} id={formId} className={styles.regForm}>
+					<LabeledInput name='Name' isInvalid={nameIsInvalid} onChange={handleNameChange} inputClassName={styles.regInput} />
+					<LabeledInput name='Email' isInvalid={emailIsInvalid} onChange={handleEmailChange} inputClassName={styles.regInput} />
+					<LabeledInput name='Password' isInvalid={passwordIsInvalid} onChange={handlePasswordChange} inputClassName={styles.regInput} />
+				</form>
+				<Button label='REGISTER' type='submit' formName={formId} className={styles.regButton} />
+				<div className={styles.regHelp}>
+					If you have an account you may{' '}
+					<Link to='/login'>
+						<b>Login</b>
+					</Link>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -6,21 +6,24 @@ import Login from './components/Login/Login';
 import Courses from './components/Courses/Courses';
 import CourseInfo from './components/CourseInfo/CourseInfo';
 import CreateCourse from './components/CreateCourse/CreateCourse';
+import { Layout } from './components/Layout';
 import App from './App';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(
 	<BrowserRouter>
-		<Routes path='/'>
-			<Route path='login' element={<Login />} />
-			<Route path='registration' element={<Registration />} />
-			<Route path='courses' element={<App />}>
-				<Route path=':courseId' element={<CourseInfo />} />
-				<Route path='add' element={<CreateCourse />} />
-				<Route index element={<Courses />} />
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index path='login' element={<Login />} />
+				<Route path='registration' element={<Registration />} />
+				<Route path='courses' element={<App />}>
+					<Route path=':courseId' element={<CourseInfo />} />
+					<Route path='add' element={<CreateCourse />} />
+					<Route index element={<Courses />} />
+				</Route>
+				<Route path='*' element={<Navigate to='/login' />} />
 			</Route>
-			<Route path='*' element={<Navigate to='/login' />} />
 		</Routes>
 	</BrowserRouter>
 );
