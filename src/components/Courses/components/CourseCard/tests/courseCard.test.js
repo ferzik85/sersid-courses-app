dd211d0037fb56654feb5002579ed15be9ae5ed2
ferzik85 <sersid@bk.ru>
2023-11-28 14:38:22 +0100

    self review 2

--- src/components/Courses/components/CourseCard/tests/courseCard.test.js
+++ src/components/Courses/components/CourseCard/tests/courseCard.test.js
@@ -6,62 +6,41 @@ import { render, screen } from '@testing-library/react';
 import { TestProvider } from '../../../../../test';
 import CourseCard from '../CourseCard';
 
+const title = 'dummy-title';
+const description = 'dummy-description';
+const creationDate = '24/11/2023';
+const authorsList = ['author1', 'author2'];
+const duration = 120;
+
+beforeEach(() => {
+	render(
+		<TestProvider>
+			<CourseCard title={title} description={description} creationDate={creationDate} duration={duration} authors={authorsList} id='' />
+		</TestProvider>
+	);
+});
+
 describe('CourseCard tests', () => {
 	test('CourseCard should display title', () => {
-		const expectedTitle = 'dummy-title';
-		render(
-			<TestProvider>
-				<CourseCard title={expectedTitle} id='' description='' creationDate='' duration={0} authors={[]} />
-			</TestProvider>
-		);
-
-		expect(screen.queryByText(expectedTitle)).toBeInTheDocument();
+		expect(screen.queryByText(title)).toBeInTheDocument();
 	});
 
 	test('CourseCard should display description', () => {
-		const expectedDescription = 'dummy-description';
-		render(
-			<TestProvider>
-				<CourseCard description={expectedDescription} id='' title='' creationDate='' duration={0} authors={[]} />
-			</TestProvider>
-		);
-
-		expect(screen.queryByText(expectedDescription)).toBeInTheDocument();
+		expect(screen.queryByText(description)).toBeInTheDocument();
 	});
 
 	test('CourseCard should display creation date in the correct format', () => {
-		const creationDate = '24/11/2023';
 		const expectedCreationDate = '24.11.2023';
-		render(
-			<TestProvider>
-				<CourseCard creationDate={creationDate} id='' description='' title='' duration={0} authors={[]} />
-			</TestProvider>
-		);
-
 		expect(screen.queryByText(expectedCreationDate)).toBeInTheDocument();
 	});
 
 	test('CourseCard should display authors list', () => {
-		const authorsList = ['author1', 'author2'];
 		const expectedAuthorsList = 'author1, author2';
-		render(
-			<TestProvider>
-				<CourseCard authors={authorsList} id='' description='' title='' creationDate='' duration={0} />
-			</TestProvider>
-		);
-
 		expect(screen.queryByText(expectedAuthorsList)).toBeInTheDocument();
 	});
 
 	test('CourseCard should display duration in the correct format', () => {
-		const duration = 120;
 		const expectedDuration = '02:00 hours';
-		render(
-			<TestProvider>
-				<CourseCard duration={duration} id='' description='' title='' creationDate='' authors={[]} />
-			</TestProvider>
-		);
-
 		expect(screen.queryByText(expectedDuration)).toBeInTheDocument();
 	});
 });
